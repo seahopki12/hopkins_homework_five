c5e3e7771dcab2627e4d83767f4573cfa73d77ba
Sean Hopkins <seahopki12@gmail.com>
2020-09-13 22:29:18 -0400

    Saved info to storage, but doesn't stay in input field after refresh.

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
+
 var currentDay = moment().format("dddd, MMMM Do");
+var eventEntry = document.getElementById("newEvent"); 
 $("#currentDay").text(currentDay);
+$("#button-addon2").on("click", function(){
+    event.preventDefault();
+    localStorage.setItem("event entry", eventEntry.value);
+});
