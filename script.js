16808d28064b69e1404d29d89107be0a722d900b
Sean Hopkins <seahopki12@gmail.com>
2020-09-15 19:37:33 -0400

    Figured out Color Coordination issue.

--- script.js
+++ script.js
@@ -1,22 +1,58 @@
 // Current Day and Time Table Hours
-var currentDay = moment().format("dddd, MMMM Do"); 
+var currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").text(currentDay);
 
-// Color Coordination
 
 
 // Declaring Variables for event entries
 var eventEntry = {
-    nine: document.getElementById("nineEvent"),
-    ten: document.getElementById("tenEvent"),
-    eleven: document.getElementById("elevenEvent"),
-    twelve: document.getElementById("twelveEvent"),
-    one: document.getElementById("oneEvent"),
-    two: document.getElementById("twoEvent"),
-    three: document.getElementById("threeEvent"),
-    four: document.getElementById("fourEvent"),
-    five: document.getElementById("fiveEvent")
+    nine: document.getElementById("9"),
+    ten: document.getElementById("10"),
+    eleven: document.getElementById("11"),
+    twelve: document.getElementById("12"),
+    one: document.getElementById("13"),
+    two: document.getElementById("14"),
+    three: document.getElementById("15"),
+    four: document.getElementById("16"),
+    five: document.getElementById("17")
 };
+
+// Color Coordination
+var idNames = [eventEntry.nine, eventEntry.ten, eventEntry.eleven, eventEntry.twelve, eventEntry.one, eventEntry.two, eventEntry.three, eventEntry.four, eventEntry.five]
+var exactHour = moment().hour();
+console.log(exactHour);
+
+for (i = 0; i < idNames.length; i++) {
+    if (exactHour == idNames[i].id) {
+        $(idNames[i]).css("background-color", "red");
+    } else if (exactHour > idNames[i].id) {
+        $(idNames[i]).css("background-color", "grey");
+    } else {
+        $(idNames[i]).css("background-color", "green");
+    };
+};
+// var values = {
+//     hours: ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"],
+//     elements: [eventEntry.nine, eventEntry.ten, eventEntry.eleven, eventEntry.twelve, eventEntry.one, eventEntry.two, eventEntry.three, eventEntry.four, eventEntry.five]
+// };
+
+// for (var i = 0; i < values.hours.length; i++) {
+//     if (moment().isBefore(values.hours[i])) {
+//         $(values.elements[i]).css("background-color", "green")
+//     } else if (moment().isAfter(values.hours[i])) {
+//         $(values.elements[i]).css("background-color", "grey")
+//     } else {
+//         $(values.elements[i]).css("background-color", "red")
+//     };
+// };
+
+// if (moment().isBefore('9:00', 'hour')) {
+//     $("#nineEvent").css("background-color", "green");
+// } else if (moment().isAfter('9:00', 'hour')) {
+//     $("#nineEvent").css("background-color", "grey");
+// } else {
+//     $("#nineEvent").css("background-color", "red");
+// };
 
 // Recalling the local storage to the HTML input field
 eventEntry.nine.value = localStorage.getItem("nine entry");
@@ -30,30 +66,30 @@ eventEntry.four.value = localStorage.getItem("four entry");
 eventEntry.five.value = localStorage.getItem("five entry");
 
 // Button events for saving event to local storage
-$(".nine").on("click", function(){
+$(".nine").on("click", function () {
     localStorage.setItem("nine entry", eventEntry.nine.value);
 });
-$(".ten").on("click", function(){
+$(".ten").on("click", function () {
     localStorage.setItem("ten entry", eventEntry.ten.value);
 });
-$(".eleven").on("click", function(){
+$(".eleven").on("click", function () {
     localStorage.setItem("eleven entry", eventEntry.eleven.value);
 });
-$(".twelve").on("click", function(){
+$(".twelve").on("click", function () {
     localStorage.setItem("twelve entry", eventEntry.twelve.value);
 });
-$(".one").on("click", function(){
+$(".one").on("click", function () {
     localStorage.setItem("one entry", eventEntry.one.value);
 });
-$(".two").on("click", function(){
+$(".two").on("click", function () {
     localStorage.setItem("two entry", eventEntry.two.value);
 });
-$(".three").on("click", function(){
+$(".three").on("click", function () {
     localStorage.setItem("three entry", eventEntry.three.value);
 });
-$(".four").on("click", function(){
+$(".four").on("click", function () {
     localStorage.setItem("four entry", eventEntry.four.value);
 });
-$(".five").on("click", function(){
+$(".five").on("click", function () {
     localStorage.setItem("five entry", eventEntry.five.value);
 });
