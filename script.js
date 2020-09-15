27be0ac3c06a2994a0a062bb88fcf32c1eadffc9
Sean Hopkins <seahopki12@gmail.com>
2020-09-14 21:27:32 -0400

    Need to figure out color coordination.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-// Current Day
+// Current Day and Time Table Hours
 var currentDay = moment().format("dddd, MMMM Do"); 
 $("#currentDay").text(currentDay);
 
+// Color Coordination
+
+
+// Declaring Variables for event entries
 var eventEntry = {
     nine: document.getElementById("nineEvent"),
     ten: document.getElementById("tenEvent"),
@@ -13,8 +17,8 @@ var eventEntry = {
     four: document.getElementById("fourEvent"),
     five: document.getElementById("fiveEvent")
 };
-// var eventEntry = document.getElementById("newEvent");
 
+// Recalling the local storage to the HTML input field
 eventEntry.nine.value = localStorage.getItem("nine entry");
 eventEntry.ten.value = localStorage.getItem("ten entry");
 eventEntry.eleven.value = localStorage.getItem("eleven entry");
@@ -25,6 +29,7 @@ eventEntry.three.value = localStorage.getItem("three entry");
 eventEntry.four.value = localStorage.getItem("four entry");
 eventEntry.five.value = localStorage.getItem("five entry");
 
+// Button events for saving event to local storage
 $(".nine").on("click", function(){
     localStorage.setItem("nine entry", eventEntry.nine.value);
 });
